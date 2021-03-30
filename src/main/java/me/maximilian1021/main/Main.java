9ea8cbb071e50594cb879df0bdc65b730a032f63
Maximilian1021 <Maximilian.Preger@gmx.de>
2021-03-30 16:54:01 +0200

    feat: Added LuckPerms API and Chatformat

--- src/main/java/me/maximilian1021/main/Main.java
+++ src/main/java/me/maximilian1021/main/Main.java
@@ -1,18 +1,31 @@
 package me.maximilian1021.main;
 
 import me.maximilian1021.events.onJoin;
+import me.maximilian1021.events.tablistener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main plugin;
+    public static Main getPlugin() {
+        return plugin;
+    }
+
+    public final boolean isPlaceholderAPIEnabled() {
+        return this.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
+    }
+
     @Override
     public void onEnable() {
         PluginManager pm = Bukkit.getServer().getPluginManager();
 
+        Main.plugin = this;
+
 
         pm.registerEvents(new onJoin(), this);
+        pm.registerEvents(new tablistener(), this);
 
     }
 
